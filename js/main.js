cbf324adeab754b072f8e79d0af3eedbc499fe64
Dllanw <dcreyes0920@gmail.com>
2024-05-21 23:37:19 -0600

    add main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btnAnswer = document.getElementById('btn');
+    const btnImg = document.getElementById('btn-img');
+
+    btnAnswer.addEventListener('click', function(e) {
+        btnImg.classList.add = ('');
+        
+    })
+
+})
