c1053efdbbf984185ff91080292fe4eb583e5865
Dllanw <dcreyes0920@gmail.com>
2024-05-21 23:44:37 -0600

    eventListener answer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnAnswer = document.getElementById('btn');
     const btnImg = document.getElementById('btn-img');
+    const answer = document.getElementById('answer');
 
     btnAnswer.addEventListener('click', function(e) {
-        btnImg.classList.add = ('');
+        console.log('...');
         
     })
+
 
 })
