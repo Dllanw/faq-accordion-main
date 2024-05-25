e84037644a1fb789c6d545aa4f9b73e5c8b2f77e
Dllanw <dcreyes0920@gmail.com>
2024-05-24 20:15:19 -0600

    js complete

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btnAnswers = document.querySelectorAll(".faq-btn");
+  //   const btnImg = document.querySelector("img");
 
   btnAnswers.forEach((btnAnswer) => {
-      btnAnswer.addEventListener("click", toggleAnswer);
+    btnAnswer.addEventListener("click", toggleAnswer);
   });
 
   function toggleAnswer(e) {
-      const btn = e.target;
-      const faqContainer = btn.closest(".faq"); // Encuentra el contenedor de la pregunta y respuesta
-      const answer = faqContainer.querySelector(".faq-answer"); // Encuentra la respuesta dentro del contenedor
+    const btn = e.target;
+    const faqContainer = btn.closest(".faq"); // Encuentra el contenedor de la pregunta y respuesta
+    const btnImg = faqContainer.querySelector(".btn-img"); //Encuentra el contenedor de la imagen utilizando la clase de css
+    const answer = faqContainer.querySelector(".faq-answer"); // Encuentra la respuesta dentro del contenedo con la clase de css
 
-      answer.classList.toggle("display-none"); // Alterna la visibilidad de la respuesta
+    answer.classList.toggle("display-none"); // Alterna la visibilidad de la respuesta
 
-      // Alternar la imagen del botón
-      const btnImg = btn.querySelector("img");
-      if (btnImg) {
-          const currentSrc = btnImg.src;
-          console.log(currentSrc);
-          if (currentSrc.includes("icon-plus.svg")) {
-              btnImg.src = currentSrc.replace("icon-plus.svg", "icon-minus.svg");
-          } else {
-              btnImg.src = currentSrc.replace("icon-minus.svg", "icon-plus.svg");
-          }
+    // Alternar la imagen del botón
+
+    if (btnImg) {
+      //Verifica que el contenedor de la imagen tenga algo
+      const currentSrc = btnImg.src;
+      if (currentSrc.includes("icon-plus.svg")) {
+        // Si el contenido del elemento es icon-plus.svg entondes lo cambia a icon-minus.svg y viceversa
+        btnImg.src = currentSrc.replace("icon-plus.svg", "icon-minus.svg"); // Remplaza icon-plus.svg por icon-minus.svg
+      } else {
+        btnImg.src = currentSrc.replace("icon-minus.svg", "icon-plus.svg"); // Remplaza icon-minus.svg por icon-plus.svg
       }
+    }
   }
 });
